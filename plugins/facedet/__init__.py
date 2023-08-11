c3f3ea09aedaf1f51df1f23448041b753fdd76cc
zhuth <zthpublic@gmail.com>
2023-08-11 08:35:55 +0800

    facedet & requirements update

--- plugins/facedet/__init__.py
+++ plugins/facedet/__init__.py
@@ -1,42 +1,49 @@
 """人脸检测"""
 import base64
-from email.mime import image
 from io import BytesIO
-from bson import ObjectId
-from PIL import Image
+
 import numpy as np
+from PIL import Image
 
-from jindai import Plugin
 from jindai.helpers import safe_import
-from jindai.models import MediaItem, Paragraph, F, Fn, Var, DbObjectCollection
+from jindai.models import DbObjectCollection, F, Fn, MediaItem, Paragraph, Var
+from plugins.hashing import HashingBase, bitcount, to_int, whash
 from plugins.imageproc import MediaItemStage
-from plugins.hashing import single_item, bitcount, to_int, whash, HashingBase
 
-
-safe_import('deepface')
 
 class FaceDet(MediaItemStage):
     """人脸检测"""
-    
-    def __init__(self, method="opencv") -> None:
+
+    def __init__(self, method="retinaface") -> None:
         """
         Args:
-            method (opencv|dlib|mtcnn|ssd|retinaface): Face detection method.
+            method (opencv|retinaface): Face detection method.
                 @zhs 人脸检测方法
-        """        
+        """
         self.method = method
-        from deepface.detectors.FaceDetector import detect_faces, build_model
-        self.detector = build_model(method)
+        if method == 'opencv':
+            cv2 = safe_import('cv2', 'opencv-python-headless')
+            self.face_classifier = cv2.CascadeClassifier(
+                cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        elif method == 'retinaface':
+            safe_import('retinaface', 'retina-face')
+            from retinaface import RetinaFace
+            self.face_classifier = lambda input_image: RetinaFace.extract_faces(input_image, align=True)
+            
         super().__init__()
 
     def crop_faces(self, buf):
         """Crop faces from image in buffer"""
-        from deepface.detectors.FaceDetector import detect_faces, build_model
         image = Image.open(buf)
         image.thumbnail((1024, 1024))
-        for face in detect_faces(self.detector, self.method, np.array(image)):
-            im, _ = face
-            yield Image.fromarray(im)
+        if self.method == 'opencv':
+            gray_image = np.array(image.convert('L'))
+            for (x, y, w, h) in self.face_classifier.detectMultiScale(
+                        gray_image, minSize=(40, 40)):
+                yield image.crop((x, y, x+w, y+h))
+        elif self.method == 'retinaface':
+            for im in self.face_classifier(np.array(image)):
+                yield Image.fromarray(im)
 
     def resolve_image(self, i: MediaItem, _):
         """Resolve image"""
@@ -51,7 +58,7 @@ class FaceDet(MediaItemStage):
             i.save()
         except OSError:
             pass
-        
+
         return i
 
 
@@ -60,33 +67,33 @@ class FaceDetPlugin(HashingBase):
 
     def __init__(self, pmanager, **_):
         super().__init__(pmanager)
-        
+
         self.det = FaceDet()
         self.register_pipelines([FaceDet])
         self.register_filter(
             'face', 'e', 'face/{mediaitem._id}', 'mdi-emoticon-outline', self.handle_filter)
         MediaItem.set_field('faces', DbObjectCollection(bytes))
-        
+
     def handle_filter_check(self, context):
         m = MediaItem.first(F.id == context.iid)
         if not m.faces:
             FaceDet().resolve_image(m, None)
-        
+
         if not m.faces:
             return False
-        
+
         fid = context.args[0] if len(context.args) > 0 else ''
         fid = 0 if not fid else int(fid)
-        
+
         context.fdh = [to_int(f) for f in m.faces]
         if fid:
             context.fdh = [context.fdh[fid-1]]
-            
+
         if not context.fdh:
             return []
-        
+
         paragraph_faces = context.sticky_paragraphs
-        
+
         source_paragraph = paragraph_faces[0]
         for face in self.det.crop_faces(source_paragraph.images[0].data):
             saved = BytesIO()
@@ -96,8 +103,8 @@ class FaceDetPlugin(HashingBase):
                     _id=source_paragraph.id,
                     images=[
                         MediaItem(source={
-                                    'url': 'data:image/jpeg;base64,'
-                                    + base64.b64encode(saved.getvalue()).decode('ascii')}, item_type='image')
+                            'url': 'data:image/jpeg;base64,'
+                            + base64.b64encode(saved.getvalue()).decode('ascii')}, item_type='image')
                     ]
                 )
             )
@@ -107,17 +114,17 @@ class FaceDetPlugin(HashingBase):
 
         context.sticky_paragraphs = paragraph_faces
         return True
-        
+
     def handle_filter_item(self, image_item, context):
         if not image_item or not isinstance(image_item, MediaItem) \
-            or not image_item.faces or image_item.id == context.iid:
+                or not image_item.faces or image_item.id == context.iid:
             return
         image_item.score = min([
             min([bitcount(to_int(i) ^ j) for j in context.fdh])
             for i in image_item.faces
         ])
         return image_item
-        
+
     def handle_filter(self, dbq, iid='', *args):
         """Handle page"""
         if iid == '':
